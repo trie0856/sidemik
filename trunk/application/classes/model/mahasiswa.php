89651bbc85c59eee03871c31ef4f29d130b9640d
rezanachmad@gmail.com <rezanachmad@gmail.com@2cf86c9a-6209-780b-b72d-5d54f8e0d35d>
2010-11-12 12:20:10 +0000

    tambah mahasiswa sudah bisa...

---
+++ trunk/application/classes/model/mahasiswa.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Mahasiswa extends ORM
+{
+    protected $_primary_key = 'nim';
+    protected $_table_name = 'mahasiswa';
+}
