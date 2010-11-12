934632f096e88a0a2f77d0d6c789f7c53bf183db
rezanachmad@gmail.com <rezanachmad@gmail.com@2cf86c9a-6209-780b-b72d-5d54f8e0d35d>
2010-11-12 09:58:13 +0000

    edit user dan tambah user sudah bisa

--- trunk/application/views/user/edit.php
+++ trunk/application/views/user/edit.php
@@ -1,1 +1,17 @@
 edit
+<?php echo Form::open(NULL, array('method'=>'post'));?>
+<table>
+    <tr>
+        <td><?php echo 'Username' ?></td>
+        <td><?php echo $user->username ?></td>
+    </tr>
+    <tr>
+        <td><?php echo Form::label('password', 'Password')?></td>
+        <td><?php echo Form::input('password', $user->password);?></td>
+    </tr>
+    <tr>
+        <td><?php echo Form::button('submit', 'Edit')?></td>
+        <td></td>
+    </tr>
+</table>
+<?php echo Form::close(); ?>
