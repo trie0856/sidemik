856ad2b6ab7a2fe1e11319ab3c14de34cfe51040
rezanachmad <rezanachmad@gmail.com>
2010-11-12 14:32:53 +0000

    edit matakuliah udah bisa --> tapi bagaimana caranya klo yang diubah adalah kodenya? (thinking)

--- application/views/matakuliah/edit.php
+++ application/views/matakuliah/edit.php
@@ -1,1 +1,33 @@
 edit
+<?php echo Form::open(NULL, array('method'=>'post'));?>
+<table>
+    <tr>
+        <td><?php echo Form::label('kode', 'Kode')?></td>
+        <td><?php echo Form::input('kode', $matakuliah->kode);?></td>
+    </tr>
+    <tr>
+        <td><?php echo Form::label('nama', 'Nama Mata Kuliah')?></td>
+        <td><?php echo Form::input('nama', $matakuliah->nama);?></td>
+    </tr>
+    <tr>
+        <td><?php echo Form::label('deskripsi', 'Deskripsi')?></td>
+        <td><?php echo Form::textarea('deskripsi', $matakuliah->deskripsi);?></td>
+    </tr>
+    <tr>
+        <td><?php echo Form::label('tingkat', 'Tingkat')?></td>
+        <td><?php echo Form::select('tingkat', array('0'=>'','1'=>'1', '2'=>'2', '3'=>'3'), $matakuliah->tingkat);?></td>
+    </tr>
+    <tr>
+        <td><?php echo Form::label('semester_buka', 'Semester Buka')?></td>
+        <td><?php echo Form::select(
+                'semester_buka',
+                array('0'=>'Semester ganjil dan genap','1'=>'Semester ganjil', '2'=>'Semester genap'),
+                $matakuliah->semester_buka
+                );?>
+        </td>
+    </tr>
+    <tr>
+        <td><?php echo Form::button('submit', 'Tambah')?></td>
+        <td></td>
+    </tr>
+</table>
