b9d2967ed072152d60a82f9a74d888348f5d9c73
rezanachmad <rezanachmad@gmail.com>
2010-11-13 08:29:09 +0000

    modifikasi

--- application/classes/controller/pengambilanmatakuliah.php
+++ application/classes/controller/pengambilanmatakuliah.php
@@ -14,8 +14,32 @@ class Controller_Pengambilanmatakuliah extends Controller_Website {
         $this->template->title = "Pengambilan matakuliah";
     }
 
-    public function action_ambil($nim = NULL) {
+    public function action_ambil($nim) {
         $this->template->title = "Ambil matakuliah";
+        $matakuliahs = new Model_Matakuliah();
+        $mahasiswa = new Model_Mahasiswa($nim);
+        $pengambilanmks = new Model_Pengambilanmk();
+
+        $this->template->content->matakuliahs = $matakuliahs->find_all();
+        $this->template->content->mahasiswa = $mahasiswa;
+        $this->template->content->pengambilanmks = $pengambilanmks;
+    }
+
+    public function action_ksm($nim) {
+        $this->template->title = "KSM";
+        $matakuliahs = array();
+        $pengambilanmks = new Model_Pengambilanmk();
+        $pengambilanmks = $pengambilanmks
+                            ->where('nim_mahasiswa', '=', $nim)
+                            ->where('semester', '=', 2)
+                            ->find_all();
+        foreach ($pengambilanmks as $pengambilanmk) {
+            $kode = $pengambilanmk->kode_kuliah;
+            $matakuliah = new Model_Matakuliah($kode);
+            $matakuliahs[] = $matakuliah;
+        }
+
+        $this->template->content->matakuliahs = $matakuliahs;
     }
 
     public function action_transkrip() {
