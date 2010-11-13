d786fb65cc69d1c491f84d8c4691b0c4d71bc2ff
rezanachmad@gmail.com <rezanachmad@gmail.com@2cf86c9a-6209-780b-b72d-5d54f8e0d35d>
2010-11-13 01:53:22 +0000

    view dan list matakuliah sudah ada

---
+++ trunk/application/views/matakuliah/view.php
@@ -0,0 +1,27 @@
+view
+<table>
+    <tr>
+        <td><?php echo 'Kode';?></td>
+        <td><?php echo $matakuliah->kode;?></td>
+    </tr>
+    <tr>
+        <td><?php echo 'Nama Mata Kuliah';?></td>
+        <td><?php echo $matakuliah->nama;?></td>
+    </tr>
+    <tr>
+        <td><?php echo 'Jumlah SKS'?></td>
+        <td><?php echo $matakuliah->jumlah_sks;?></td>
+    </tr>
+    <tr>
+        <td><?php echo 'Deskripsi'?></td>
+        <td><?php echo $matakuliah->deskripsi;?></td>
+    </tr>
+    <tr>
+        <td><?php echo 'Tingkat'?></td>
+        <td><?php echo $matakuliah->tingkat;?></td>
+    </tr>
+    <tr>
+        <td><?php echo 'Semester Buka'?></td>
+        <td><?php echo $matakuliah->semester_buka;?></td>
+    </tr>
+</table>
