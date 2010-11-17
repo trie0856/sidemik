c151c5c08cd9d8b94bb004ebb012b5fc1f5b5007
rezanachmad <rezanachmad@gmail.com>
2010-11-17 12:12:46 +0000

    inputnilai udah bisa

--- application/classes/controller/pengambilanmatakuliah.php
+++ application/classes/controller/pengambilanmatakuliah.php
@@ -76,10 +76,94 @@ class Controller_Pengambilanmatakuliah extends Controller_Website {
 
     public function action_transkrip() {
         $this->template->title = "Transkrip mahasiswa";
+
+        
     }
 
-    public function action_inputnilai($kode_mata_kuliah = NULL) {
-        $this->template->title = "Inputnilai mahasiswa";
+    public function action_inputnilai($kode_mata_kuliah = NULL, $tahun = NULL, $semester = NULL) {
+        $this->template->title = "Input Nilai Mahasiswa";
+        $request = Request::instance();
+
+        if (isset($_POST['pilih']) && $_POST['tahun']!='0' &&
+                $_POST['semester']!='0' && $_POST['matakuliah']!='0') {
+            $request->redirect($request->controller . '/' . $request->action .
+                    '/' . $_POST['matakuliah'] . '/' . $_POST['tahun'] . '/' .
+                    $_POST['semester']);
+        }
+
+        if (isset($_POST['simpan'])) {
+            foreach($_POST['nilai'] as $nim => $nilai) {
+                $mahasiswa = new Model_Mahasiswa($nim);
+                $_sems = $this->tahun_semester_to_semester($tahun,
+                        $semester, $mahasiswa->tahun_masuk);
+                $id = array(
+                    'nim_mahasiswa' => $nim,
+                    'kode_kuliah'   => $kode_mata_kuliah,
+                    'semester'      => $_sems
+                );
+                $pmk = new Model_Pengambilanmk($id);
+                $pmk->nilai = $nilai;
+                $pmk->save();
+            }
+        }
+
+        $matakuliahs = new Model_Matakuliah();
+        $matakuliahs = $matakuliahs->find_all();
+        $list_matakuliah = array();
+        $list_matakuliah[0] = ' ';
+        foreach ($matakuliahs as $matakuliah) {
+            $list_matakuliah[$matakuliah->kode] = $matakuliah->kode . ' ' .
+                    $matakuliah->nama;
+        }
+
+        $list_tahun = array();
+        $list_tahun[0] = ' ';
+        $tahun_sekarang = date('Y');
+        for ($t = $tahun_sekarang - 4; $t <= $tahun_sekarang + 4; ++$t) {
+            $list_tahun[$t] = $t;
+        }
+
+        $list_input_nilai = array();
+        if ($kode_mata_kuliah != NULL && $tahun != NULL && $semester != NULL) {
+            //echo $kode_mata_kuliah;
+            $pengambilan_mks = new Model_Pengambilanmk();
+            $pengambilan_mks = $pengambilan_mks->
+                    where('kode_kuliah', '=', $kode_mata_kuliah)->
+                    order_by('nim_mahasiswa', 'ASC')->find_all();
+            foreach($pengambilan_mks as $p_mk) {
+                $mahasiswa = new Model_Mahasiswa($p_mk->nim_mahasiswa);
+                $convert_result = ($tahun - $mahasiswa->tahun_masuk) * 2 + $semester;
+                if ($convert_result == $p_mk->semester) {
+                    $nilai;
+                    if ($p_mk->nilai == NULL) {
+                        $nilai = 0;
+                    } else {
+                        $nilai = $p_mk->nilai;
+                    }
+                    
+                    $list_input_nilai[] = array (
+                        'nim'   => $mahasiswa->nim,
+                        'nama'  => $mahasiswa->nama,
+                        'nilai' => $nilai
+                    );
+                }
+            }
+
+            $this->template->content->selected_matakuliah = $kode_mata_kuliah;
+            $this->template->content->selected_tahun = $tahun;
+            $this->template->content->selected_semester = $semester;
+        } else {
+            $this->template->content->selected_matakuliah = 0;
+            $this->template->content->selected_tahun = 0;
+            $this->template->content->selected_semester = 0;
+        }
+        $this->template->content->list_input_nilai = $list_input_nilai;
+        $this->template->content->list_matakuliah = $list_matakuliah;
+        $this->template->content->list_tahun = $list_tahun;
+    }
+
+    public function tahun_semester_to_semester($tahun, $semester, $tahun_masuk) {
+        return ($tahun - $tahun_masuk) * 2 + $semester;
     }
 
     public function action_jadwal($nim) {
--- application/views/pengambilanmatakuliah/inputnilai.php
+++ application/views/pengambilanmatakuliah/inputnilai.php
@@ -1,1 +1,91 @@
-inputnilai
+<?php echo Form::open(NULL, array('method' => 'post'))?>
+<fieldset>
+    <legend>Pilih Matakuliah dan Tahun</legend>
+    <table>
+        <tr>
+            <td>Mata Kuliah</td>
+            <td><?php echo Form::select('matakuliah', $list_matakuliah, $selected_matakuliah)?></td>
+        </tr>
+        <tr>
+            <td>Tahun</td>
+            <td><?php echo Form::select('tahun', $list_tahun, $selected_tahun)?></td>
+        </tr>
+        <tr>
+            <td>Semester</td>
+            <td>
+                <?php
+                $list_semester = array(
+                    '0' => ' ',
+                    '1' => 'Ganjil',
+                    '2' => 'Genap'
+                );
+                echo Form::select('semester', $list_semester, $selected_semester);
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2"><?php echo Form::submit('pilih', 'Pilih') ?></td>
+        </tr>
+    </table>
+</fieldset>
+<?php echo Form::close()?>
+
+
+<?php
+if (count($list_input_nilai) > 0) {
+echo Form::open(NULL, array('method' => 'post'));
+?>
+<fieldset>
+    <legend>
+        <?php echo $list_matakuliah[$selected_matakuliah] . " - " . 
+        $selected_tahun . " - Semeter " . $list_semester[$selected_semester];
+        ?>
+    </legend>
+    <table>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>NIM</th>
+                <th>Nama Mahasiswa</th>
+                <th>Nilai</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <td colspan="4"><?php echo Form::submit('simpan', 'Simpan')?></td>
+            </tr>
+        </tfoot>
+        <tbody>
+            <?php
+            $no = 0;
+            foreach ($list_input_nilai as $lin) {
+            ++$no;
+            ?>
+            <tr>
+                <td><?php echo $no; ?></td>
+                <td><?php echo $lin['nim']; ?></td>
+                <td><?php echo $lin['nama']; ?></td>
+                <td>
+                <?php
+                $list_nilai = array (
+                    '0' => ' ',
+                    'A' => 'A',
+                    'B' => 'B',
+                    'C' => 'C',
+                    'D' => 'D',
+                    'E' => 'E',
+                );
+                echo Form::select('nilai['.$lin['nim'].']', $list_nilai, $lin['nilai']);
+                ?>
+                </td>
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+</fieldset>
+<?php
+echo Form::close();
+}
+?>
