1877a8aa37db15b9fa5540326a7e5ced6447ba48
rezanachmad@gmail.com <rezanachmad@gmail.com@2cf86c9a-6209-780b-b72d-5d54f8e0d35d>
2010-11-18 14:57:20 +0000

    status pembayaran mahasiswa

---
+++ trunk/application/classes/model/statuspembayaran.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Statuspembayaran extends ORM
+{
+    protected $_table_name = 'status_pembayaran';
+}
--- trunk/application/views/mahasiswa/statuspembayaran.php
+++ trunk/application/views/mahasiswa/statuspembayaran.php
@@ -1,1 +1,29 @@
-status Pembayaran
+<fieldset style="width: 300px; margin-left: auto; margin-right: auto;">
+    <legend>Status Pembayaran</legend>
+    <?php echo Form::open(NULL);?>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Semester</th>
+                <th>Sudah Membayar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            for ($i = 1; $i <= 6; ++$i) :
+            ?>
+            <tr>
+                <td><?php echo $i;?></td>
+                <td>
+                    <?php
+                    echo Form::checkbox("bayar[$i]", 1, (bool)  in_array($i, $bayar));
+                    ?>
+                </td>
+            </tr>
+            <?php endfor;?>
+        </tbody>
+    </table>
+    <br />
+    <?php echo Form::submit('simpan', 'Simpan')?>
+    <?php echo Form::close();?>
+</fieldset>
