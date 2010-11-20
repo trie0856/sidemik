d72befd0dc2225ffd66aba6069878871723e1429
rezanachmad@gmail.com <rezanachmad@gmail.com@2cf86c9a-6209-780b-b72d-5d54f8e0d35d>
2010-11-20 07:53:44 +0000

    bug manage_greeting untuk dosen

--- trunk/application/views/user/edit.php
+++ trunk/application/views/user/edit.php
@@ -1,21 +1,20 @@
-edit
 <?php echo Form::open(NULL, array('method'=>'post'));?>
 <table>
     <tr>
-        <td><?php echo 'Username' ?></td>
+        <td width="150"><?php echo 'Username' ?></td>
         <td><?php echo $user->username ?></td>
     </tr>
     <tr>
         <td><?php echo Form::label('password', 'Password')?></td>
-        <td><?php echo Form::input('password');?></td>
+        <td><?php echo Form::password('password');?></td>
     </tr>
     <tr>
         <td><?php echo Form::label('retype_password', 'Konfirmasi Password')?></td>
-        <td><?php echo Form::input('retype_password');?></td>
+        <td><?php echo Form::password('retype_password');?></td>
     </tr>
     <tr>
-        <td><?php echo Form::button('submit', 'Edit')?></td>
         <td></td>
+        <td><?php echo Form::button('submit', 'Ubah')?></td>
     </tr>
 </table>
 <?php echo Form::close(); ?>
